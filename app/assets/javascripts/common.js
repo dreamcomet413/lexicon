57f54143782acc70825fa178896f5223cd6eb126
Hemant Khemani <contacthrk@gmail.com>
2017-03-31 13:47:54 +0530

    make order qty field readonly, once lock is unlocked don't allow lock again, minor ui changes

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -3,19 +3,20 @@ $(document).ready(function(){
   $("a.lock-icon").click(function(e) {
     e.preventDefault();
     var el = $(this);
-    el.toggleClass('unlock');
     
     var input = el.siblings(".quantity-input").first();
-    if(el.hasClass('unlock')){
+    if(!el.hasClass('unlock')){
+      el.addClass('unlock');
       input.attr("max", 999999);
       input.tooltipster('open');
       input.addClass('mark-input-red');
-    } else {
-      input.attr("max", input.data("max-copy"));
-      input.val(input.attr('min'));
-      input.tooltipster('close');
-      input.removeClass('mark-input-red');
-    }
+      input.attr("readonly", false);
+    } // else {
+     //      input.attr("max", input.data("max-copy"));
+     //      input.val(input.attr('min'));
+     //      input.tooltipster('close');
+     //      input.removeClass('mark-input-red');
+     //    }
     return false;
   });
   
