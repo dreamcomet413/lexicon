e4af3eb4dbe106dcb48b877ad49901045f612d8a
Hemant Khemani <contacthrk@gmail.com>
2017-03-31 13:31:31 +0530

    removed basic auth

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,5 @@
 class OrdersController < ApplicationController
-  http_basic_authenticate_with name: "safe", password: "qwerty!@"
+  # http_basic_authenticate_with name: "safe", password: "qwerty!@"
   
   layout false
   
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  http_basic_authenticate_with name: "safe", password: "qwerty!@"
+  # http_basic_authenticate_with name: "safe", password: "qwerty!@"
   
   def home
     
